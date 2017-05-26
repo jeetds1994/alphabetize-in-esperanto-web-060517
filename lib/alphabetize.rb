ba2bd53111a73722c6764125568b33998bf8f9a9
Diljeet <jeetds1994@gmail.com>
2017-05-25 23:08:09 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
   # code here
+
+  #credit https://stackoverflow.com/questions/28368857/how-to-sort-an-array-of-accented-words-in-ruby
+
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  arr.sort_by do |word|
+    word.chars.map {|c| alpha.index(c)}
+  end
 end
